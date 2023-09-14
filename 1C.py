3fdadbb463f201cd4c719c6c6987254ffb278c88
JesusHaans <jesuslopez@ciencias.unam.mx>
2023-09-13 21:15:17 -0500

    Terminando el problema de la biparticion con python

--- 1C.py
+++ 1C.py
@@ -1,5 +1,11 @@
+#imports
+import random
+
 # Variables globales
 FILE_NAME = "ENTRADA1.txt"
+#particiones de V
+P1=[]
+P2=[]
 
 #Abrimos el archivo
 file = open(FILE_NAME)
@@ -18,7 +24,33 @@ while (linea != ''):
 #Trabajamos str Vertices
 vertices = strVertices.split(',')
 
+#Hagamos la fase adivinadora
 
-print(vertices)
-print(aristas[0])
-print(aristas[len(aristas)-1])
+def faseAdivinadora(vertices):
+    for v in vertices:
+        i = random.randint(0,100)
+        if(i%2 == 0):
+            P1.append(v)
+        else:
+            P2.append(v)
+    print("Terminando la fase adivinadora las particiones quedan asi...")
+    print("Particion P1 = " + ' '.join(P1))
+    print("Particion P2 = " + ' '.join(P2))
+
+def faseVerificadora(P1,P2,aristas):
+    resultado = verificaAdyacencia(P1,aristas) and  verificaAdyacencia(P2,aristas)
+    print("¿P1 y P2 son una biparticion de G?")
+    print(resultado)
+
+def verificaAdyacencia(P1,aristas):
+    resultado = True
+    for vi in P1:
+        for vj in P1:
+            if (vi,vj) in aristas:
+                resultado = False
+    return resultado
+
+
+
+faseAdivinadora(vertices)
+faseVerificadora(P1,P2,aristas)
